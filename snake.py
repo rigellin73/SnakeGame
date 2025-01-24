21c2147c3edb2560b276b8c6e34d764123af6f8a
rigellin73 <rigellin73@gmail.com>
2025-01-24 12:43:11 +0100

    move constants in init function and add some comments in methods

--- snake.py
+++ snake.py
@@ -3,34 +3,39 @@ from turtle import Turtle
 class Snake:
 
     def __init__(self):
+
+        # Constants
+        self.START_SNAKE_LEN = 3
+        self.START_POS_X = 0
+        self.START_POS_Y = self.START_POS_X
+        self.TURTLE_SIDE_LEN = 20
+        self.TURTLE_SHAPE = "square"
+        self.TURTLE_COLOR = "white"
+        self.SNAKE_STEP = 20
+        self.DIRECTION_ANGLE = {
+            "EAST": 0,
+            "NORTH": 90,
+            "WEST": 180,
+            "SOUTH": 270
+        }
+
+        # Create default snake
         for snake_part_num in range(self.START_SNAKE_LEN):
             new_snake_part = self.create_snake_part()
             new_snake_part.goto(self.START_POS_X - self.TURTLE_SIDE_LEN * snake_part_num, self.START_POS_Y)
             self.snake.insert(0, new_snake_part)
 
-    START_SNAKE_LEN = 3
-    START_POS_X = 0
-    START_POS_Y = START_POS_X
-    TURTLE_SIDE_LEN = 20
-    TURTLE_SHAPE = "square"
-    TURTLE_COLOR = "white"
-    SNAKE_STEP = 20
-    DIRECTION_ANGLE = {
-        "EAST": 0,
-        "NORTH": 90,
-        "WEST": 180,
-        "SOUTH": 270
-    }
-
     snake = []
 
     def create_snake_part(self):
+        """Create single snake segment with default settings and return it"""
         snake_part = Turtle(self.TURTLE_SHAPE)
         snake_part.color(self.TURTLE_COLOR)
         snake_part.penup()
         return snake_part
 
     def move(self):
+        """Move the snake one step"""
         for index, segment in enumerate(self.snake):
             if index < len(self.snake) - 1:
                 next_segment = self.snake[index + 1]
@@ -40,4 +45,5 @@ class Snake:
                 segment.forward(self.SNAKE_STEP)
 
     def turn(self, direction):
+        """Turn the snake's head in one of the directions: EAST, NORTH, WEST or SOUTH"""
         self.snake[-1].setheading(self.DIRECTION_ANGLE[direction])
