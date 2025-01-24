6f366f9041645688cce791ddb34791b74cc928f8
rigellin73 <rigellin73@gmail.com>
2025-01-24 12:50:24 +0100

    move constants out of class and move snake creation in the method

--- snake.py
+++ snake.py
@@ -1,36 +1,37 @@
 from turtle import Turtle
 
+# Constants
+START_SNAKE_LEN = 3
+START_POS_X = 0
+START_POS_Y = START_POS_X
+TURTLE_SIDE_LEN = 20
+TURTLE_SHAPE = "square"
+TURTLE_COLOR = "white"
+SNAKE_STEP = 20
+DIRECTION_ANGLE = {
+    "EAST": 0,
+    "NORTH": 90,
+    "WEST": 180,
+    "SOUTH": 270
+}
+
 class Snake:
 
     def __init__(self):
-
-        # Constants
-        self.START_SNAKE_LEN = 3
-        self.START_POS_X = 0
-        self.START_POS_Y = self.START_POS_X
-        self.TURTLE_SIDE_LEN = 20
-        self.TURTLE_SHAPE = "square"
-        self.TURTLE_COLOR = "white"
-        self.SNAKE_STEP = 20
-        self.DIRECTION_ANGLE = {
-            "EAST": 0,
-            "NORTH": 90,
-            "WEST": 180,
-            "SOUTH": 270
-        }
-
         self.snake = []
+        self.create_snake()
 
-        # Create default snake
-        for snake_part_num in range(self.START_SNAKE_LEN):
+    def create_snake(self):
+        """Create snake from segments at the start"""
+        for snake_part_num in range(START_SNAKE_LEN):
             new_snake_part = self.create_snake_part()
-            new_snake_part.goto(self.START_POS_X - self.TURTLE_SIDE_LEN * snake_part_num, self.START_POS_Y)
+            new_snake_part.goto(START_POS_X - TURTLE_SIDE_LEN * snake_part_num, START_POS_Y)
             self.snake.insert(0, new_snake_part)
 
     def create_snake_part(self):
         """Create single snake segment with default settings and return it"""
-        snake_part = Turtle(self.TURTLE_SHAPE)
-        snake_part.color(self.TURTLE_COLOR)
+        snake_part = Turtle(TURTLE_SHAPE)
+        snake_part.color(TURTLE_COLOR)
         snake_part.penup()
         return snake_part
 
@@ -42,8 +43,8 @@ class Snake:
                 next_segment_pos = (next_segment.xcor(), next_segment.ycor())
                 segment.goto(next_segment_pos)
             else:
-                segment.forward(self.SNAKE_STEP)
+                segment.forward(SNAKE_STEP)
 
     def turn(self, direction):
         """Turn the snake's head in one of the directions: EAST, NORTH, WEST or SOUTH"""
-        self.snake[-1].setheading(self.DIRECTION_ANGLE[direction])
+        self.snake[-1].setheading(DIRECTION_ANGLE[direction])
